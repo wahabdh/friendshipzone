dc62b29e2d5db1e22f5388a242d2e52bdc654ed6
wahabdh <malikwahab565@gmail.com>
2026-05-20 20:43:32 +0500

    Update route.ts

--- app/api/menu/route.ts
+++ app/api/menu/route.ts
@@ -3,57 +3,85 @@ let menuItems = [
     id: 1,
     name: "Chicken Samosa",
     price: 700,
-    description: "Fresh Chicken Samosa",
+    description: "12 Pcs Fresh & home made items",
     image: "/menu-items/Chicken Samosa.jpg"
   },
   {
     id: 2,
     name: "Beef Shahmi Kabaab",
     price: 1200,
-    description: "Fresh Beef Shahmi Kabaab",
+    description: "12 Pcs Fresh & home made items",
     image: "/menu-items/Beef Shahmi Kabaab.jpg"
   },
   {
     id: 3,
     name: "Pizza Roll",
     price: 1500,
-    description: "Classic pizza Roll",
+    description: "12 Pcs Fresh & home made items",
     image: "/menu-items/Pizza Roll.jpg"
   },
    {
     id: 4,
     name: "Chicken Seekh Kabab",
     price: 1000,
-    description: "Fresh Chicken Seekh Kabab",
+    description: "12 Pcs Fresh & home made items",
     image: "/menu-items/Chicken Seekh Kabab.jpg"
   },
   {
     id: 5,
     name: "Chicken Shami Kabab",
     price: 1000,
-    description: "Fresh Chicken Shami Kabab",
+    description: "12 Pcs Fresh & home made items",
     image: "/menu-items/Chicken Shami Kabab.jpg"
   },
   {
     id: 6,
     name: "Spring Roll",
     price: 1000,
-    description: "Fresh Spring Roll",
+    description: "12 Pcs Fresh & home made items",
     image: "/menu-items/Spring Roll.jpg"
   },
   {
     id: 7,
     name: "Tikka Roll",
     price: 1000,
-    description: "Fresh Tikka Roll",
+    description: "12 Pcs Fresh & home made items",
     image: "/menu-items/Tikka Roll.jpg"
   },
   {
     id: 8,
     name: "Cheese Roll Large",
     price: 1800,
-    description: "Fresh Cheese Roll large size",
+    description: "12 Pcs Fresh & home made items",
     image: "/menu-items/Cheese Roll.jpg"
+  },
+  {
+    id: 9,
+    name: "Aloo Samosa",
+    price: 500,
+    description: "12 Pcs Fresh & home made items",
+    image: "/menu-items/Aloo Samosa.jpg"
+  },
+    {
+    id: 10,
+    name: "Potato Cheese Ball",
+    price: 1000,
+    description: "12 Pcs Fresh & home made items",
+    image: "/menu-items/Potato Cheese Ball.jpg"
+  },
+   {
+    id: 11,
+    name: "Chicken Cheese Pops",
+    price: 1000,
+    description: "12 Pcs Fresh & home made items",
+    image: "/menu-items/Chicken Cheese Pops.jpg"
+  },
+    {
+    id: 12,
+    name: "Chicken Cheese Lolly Pops",
+    price: 1000,
+    description: "12 Pcs Fresh & home made items",
+    image: "/menu-items/Chicken Cheese Lolly Pops.jpg"
   }
 ];
 
